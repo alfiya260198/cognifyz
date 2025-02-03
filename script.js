3af8d211e96a11b768b24554ccea123c4cc1ea14
alfiya260198 <alfiyaali2601@gmail.com>
2025-02-03 23:02:19 +0530

    task 5

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const button = document.getElementById("colorButton");
-function changeBackgroundColor() {
-    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        document.body.style.backgroundColor = randomColor;
+document.getElementById("loadData").addEventListener("click", fetchPost);
+function fetchPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => {
+            const randomPost = data[Math.floor(Math.random() * data.length)];
+            document.getElementById("postContainer").innerHTML = `
+                <h2>${randomPost.title}</h2>
+                <p>${randomPost.body}</p>
+            `;
+        })
+        .catch(error => console.error("Error fetching data:", error));
 }
-button.addEventListener("click", changeBackgroundColor);
